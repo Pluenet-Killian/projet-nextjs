ee9cd3fc987b26e831110b27709c3a4742aa5b1c
Killian Pluenet <killian.pluenet@gmail.com>
2023-07-13 16:44:22 +0200

    end of the code

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,6 @@
 import './globals.css'
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-
-const inter = Inter({ subsets: ['latin'] })
+import Navbar from './components/NavBar'
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -16,7 +14,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className='bg-slate-800' >
+        <Navbar/>
+        {children}
+        </body>
     </html>
   )
 }
